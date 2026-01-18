1ee86b023d1c82d825f85cae6f129351e93b70b8
7A90 <129234990+7A90@users.noreply.github.com>
2026-01-18 01:25:41 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'ma-bibliotheque-v1';
+// Liste de TOUS les fichiers à sauvegarder pour le mode hors ligne
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './livre1.pdf',
+  './livre2.pdf',
+  './livre3.pdf',
+  './livre4.pdf',
+  './livre5.pdf',
+  './livre6.pdf',
+  './icone-192.png',
+  './icone-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.min.js'
+];
+
+// Installation : on télécharge les fichiers dans le cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Interception des requêtes : on sert les fichiers du cache si on est hors ligne
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
